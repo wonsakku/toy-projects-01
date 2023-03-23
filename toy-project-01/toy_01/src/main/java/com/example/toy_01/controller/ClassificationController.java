9f7e75570f25a4283a0ed7803d6bdaceba942fca
wonsakku <kws1044@gmail.com>
2023-03-23 15:33:48 +0900

    게시글 작성

--- toy-project-01/toy_01/src/main/java/com/example/toy_01/controller/ClassificationController.java
+++ toy-project-01/toy_01/src/main/java/com/example/toy_01/controller/ClassificationController.java
@@ -1,2 +1,36 @@
-package com.example.toy_01.controller;public class ClassificationController {
+package com.example.toy_01.controller;
+
+import com.example.toy_01.domain.Classification;
+import com.example.toy_01.dto.RespClassificationDto;
+import com.example.toy_01.repository.ClassificationRepository;
+import com.example.toy_01.service.ClassificationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/classifications")
+public class ClassificationController {
+
+    private final ClassificationService classificationService;
+
+    @GetMapping("/select-box")
+    public ResponseEntity<List<RespClassificationDto>> classificationSelectBox(){
+        List<Classification> classifications = classificationService.findAll();
+
+        List<RespClassificationDto> results = classifications.stream()
+                .map(RespClassificationDto::new)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(results);
+    }
+
+
+
 }
